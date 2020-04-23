404f3d39b5e9d54fc3a6f0b59b2c8073966e60d8
Mehmet Salih Aslan <aslanmsalih@gmail.com>
2020-04-23 15:43:34 +0300

    Initial Commit

---
+++ mvvm-r/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mvvm-r
+//
+//  Created by Mehmet Salih Aslan on 23.04.2020.
+//  Copyright © 2020 Mehmet Salih Aslan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
